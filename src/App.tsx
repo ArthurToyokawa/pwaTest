380b7e0fedcf235b4eb76656d8cc54c6d6cb4b6e
Arthur Toyokawa Sperandio <arthur.sperandio@objective.com.br>
2022-10-28 11:25:44 -0300

    consertando erros no suspense

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,25 @@ const Home = lazy(() => import('./Home'));
 const App: React.FC = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
-    {/*...*/}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/about" element={ <About />}>
+        
+        </Route>
+        <Route path="/" element={<Home/>}>
+          
+        </Route>
+      </Routes>
     </Suspense>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Routes>
   </Router>
 );
 export default App;
